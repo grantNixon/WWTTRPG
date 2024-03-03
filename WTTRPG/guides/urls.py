e5e2a530873d8f513fe5249b05ee72125fb693b9
grantNixon <grntnixon1@gmail.com>
2024-03-03 11:36:13 -0600

    Linked all compendium pages to dropdown

--- WTTRPG/guides/urls.py
+++ WTTRPG/guides/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
+from .views import *
+
+app_name = 'guides'
 
 urlpatterns = [
     path('', views.guide_view),
     path('mg_purchase/', views.mg_purchase_view),
-    path('av1_purchase/',views.av1_purchase_view)
+    path('av1_purchase/',views.av1_purchase_view),
+    path('comp_Clothing-Armor',CompClothingArmor.as_view(),name='comp_clothingarmor'),
+    path('comp_backgrounds',CompBackgrounds.as_view(),name='comp_backgrounds'),
+    path('comp_MysItems-Weapons',CompMysItemsWeapons.as_view(),name='comp_mysitemsweapons'),
+    path('comp_races',CompRaces.as_view(),name='comp_races'),
+    path('comp_spells',CompSpells.as_view(),name='comp_spells'),
+    path('comp_tonics',CompTonics.as_view(),name='comp_tonics'),
+    path('comp_utilities',CompUtilities.as_view(),name='comp_utilities'),
+    path('comp_weapons',CompWeapons.as_view(),name='comp_weapons'),
 ]
--- WTTRPG/guides/views.py
+++ WTTRPG/guides/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def guide_view(request):
@@ -9,3 +10,27 @@ def mg_purchase_view(request):
 
 def av1_purchase_view(request):
     return(render(request,"guides/av1_purchase.html"))
+
+class CompBackgrounds(TemplateView):
+    template_name = 'guides/Compendium_Backgrounds.html'
+
+class CompMysItemsWeapons(TemplateView):
+    template_name = 'guides/Compendium_MysItems-Weapons.html'
+
+class CompRaces(TemplateView):
+    template_name = 'guides/Compendium_Races.html'
+
+class CompSpells(TemplateView):
+    template_name = 'guides/Compendium_Spells.html'
+
+class CompTonics(TemplateView):
+    template_name = 'guides/Compendium_Tonics.html'
+
+class CompUtilities(TemplateView):
+    template_name = 'guides/Compendium_Utilities.html'
+
+class CompWeapons(TemplateView):
+    template_name = 'guides/Compendium_Weapons.html'
+
+class CompClothingArmor(TemplateView):
+    template_name = 'guides/Compendium_Clothing-Armor.html'
