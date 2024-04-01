c5e0ab143ccc966814d45295a2f1845d68bff71b
grantNixon <grntnixon1@gmail.com>
2024-04-01 17:54:20 -0500

    attribute drop downs working now

--- WTTRPG/create/create_settings.py
+++ WTTRPG/create/create_settings.py
@@ -1,3 +1,10 @@
 
 class Races():
     races = ["Human","Armadill","Hillock","Folinn","Shibasa","Bak'wa","Egriss","Bolibar","Cincindel","Ambaazi","Palla","Latran"]
+
+class Backgrounds():
+    backgrounds = ["Cowboy", "Rancher", "Bounty Hunter", "Snake Oil Salesman", "Shopkeep", "Doctor", "Elemental Fella", "Wilderness Guide", "Worshipper", "Missionary", "Galvanism Scholar", "Chef"
+                   "Alchemist", "Ex-convict", "Carpenter","Pack Mule","Shockstone Researcher","Luddite","Assassin", "Street Rat","Cindeli Weapons"]
+    
+class PointBuy():
+    pointbuys = [30,20,15,15,10,5]
--- WTTRPG/create/models.py
+++ WTTRPG/create/models.py
@@ -4,8 +4,59 @@ from .create_settings import *
 def get_races():
     return {i: i for i in Races.races}
 
+def get_backgrounds():
+    return {i: i for i in Backgrounds.backgrounds}
+
+def get_pointbuys():
+    return {i: i for i in PointBuy.pointbuys}
+
 # Create your models here.
 class Character(models.Model):
     name = models.CharField(max_length = 30)
     race = models.CharField(max_length = 30, choices = get_races)
+    background = models.CharField(max_length = 30, choices = get_backgrounds)
+    strength = models.IntegerField(choices = "")
+    agility = models.IntegerField(choices = "")
+    intelligence = models.IntegerField(choices = "")
+    gumption = models.IntegerField(choices = "")
+    mysticism = models.IntegerField(choices = "")
+    personality = models.IntegerField(choices = "")
     
+class Race(models.Model):
+    RaceName = models.CharField(max_length = 30)
+    description = models.TextField()
+
+class Background(models.Model):
+    BackgroundName = models.CharField(max_length = 30)
+    StartingWeaponChoice = models.CharField(max_length = 100)
+    StartingSupplies = models.CharField(max_length = 100)
+    StartingBuff = models.CharField(max_length = 100)
+    StartingDebuff = models.CharField(max_length = 100)
+
+class Weapon(models.Model):
+    WeaponName = models.CharField(max_length = 100)
+    DamageType = models.CharField(max_length = 100)
+    ActionCost = models.FloatField()
+    Range = models.IntegerField()
+    Damage = models.CharField(max_length = 100)
+
+class Spell(models.Model):
+    MagicSchool = models.CharField(max_length = 100)
+    SpellType = models.CharField(max_length = 100)
+    SpellDescription = models.TextField()
+
+class Armor(models.Model):
+    ArmorName = models.CharField(max_length = 100)
+    ArmorDescription = models.TextField()
+
+class Tonics(models.Model):
+    TonicName = models.CharField(max_length = 100)
+    TonicSize = models.CharField(max_length = 100)
+    TonicDescription = models.TextField()
+
+class Utilities(models.Model):
+    UtilityName = models.CharField(max_length = 100)
+    UtilityType = models.CharField(max_length = 100)
+    ActionCost = models.FloatField()
+    Range = models.IntegerField()
+    Effect = models.TextField()
