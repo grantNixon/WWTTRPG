da8c23d666906a404c2937e79e9d9e36091bda33
grantNixon <grntnixon1@gmail.com>
2025-01-23 10:02:05 -0600

    code changes for Azure deployment

--- WTTRPG/WTTRPG/wsgi.py
+++ WTTRPG/WTTRPG/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
+settings_module = 'WTTRPG.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'WTTRPG.settings'
 
 import os
 
