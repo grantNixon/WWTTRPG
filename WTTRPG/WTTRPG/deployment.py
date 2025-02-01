9dbcbb0ae3d9650df25f6c85fc73b6dfb62f0237
grantNixon <grntnixon1@gmail.com>
2025-02-01 12:12:11 -0600

    config changes

--- WTTRPG/WTTRPG/deployment.py
+++ WTTRPG/WTTRPG/deployment.py
@@ -2,7 +2,7 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = ['gumption-dev-hmbferejfsd2c8ct.canadacentral-01.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
