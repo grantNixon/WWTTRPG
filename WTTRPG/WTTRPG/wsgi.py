0c5f9fc27eeb3868ae7a163bf47abf8966a229fb
grantNixon <grntnixon1@gmail.com>
2025-02-01 09:35:30 -0600

    wsgi fix

--- WTTRPG/WTTRPG/wsgi.py
+++ WTTRPG/WTTRPG/wsgi.py
@@ -6,9 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
+import os
+
 settings_module = 'WTTRPG.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'WTTRPG.settings'
 
-import os
 
 from django.core.wsgi import get_wsgi_application
 
