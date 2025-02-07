eb27f236494332ccb19b201a7e8948fb16514080
grantNixon <grntnixon1@gmail.com>
2025-02-07 16:03:17 -0600

    fix banner and backgrounds

--- WTTRPG/create/create_settings.py
+++ WTTRPG/create/create_settings.py
@@ -3,8 +3,8 @@ class Races():
     races = ["Valley Human", "Forest Human", " Seven-Banded Armadill", "Five-Banded Armadill", "Village Hillock", "Stone Hillock", "Short-tailed Folinn", "Long-tailed Folinn", "Smooth Shikasa", "Rigid Shikasa", "Onyx Bak'wa", "Mossy Bak'wa","Roaming Egriss", "Lotus Grove Egriss","Solid Bolibar", "Speckled Bolibar","Triadic Cincindel", "Analogous Cincindel","Anointed Ambaazi", "Lost Ambaazi", "Desert Latran", "Mesa Latran", "Long-Haired Margart", "Short-Haired Margart"]
 
 class Backgrounds():
-    backgrounds = ["Cowboy", "Rancher", "Bounty Hunter", "Snake Oil Salesman", "Shopkeep", "Doctor", "Elemental Fella", "Wilderness Guide", "Worshipper", "Missionary", "Galvanism Scholar", "Chef"
-                   "Alchemist", "Ex-convict", "Carpenter","Pack Mule","Shockstone Researcher","Luddite","Assassin", "Street Rat"]
+    backgrounds = ["Cowboy", "Bounty Hunter", "Snake Oil Salesman", "Doctor", "Elemental Fella", "Wilderness Guide", "Worshipper", "Missionary", "Chef"
+                   "Alchemist", "Ex-convict","Shockstone Scholar","Assassin",]
     
 class PointBuy():
     pointbuys = [30,20,16,15,10,5]
