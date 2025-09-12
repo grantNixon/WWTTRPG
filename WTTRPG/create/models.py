5eeffb06705bda3981729ed6d82d299a0cccc105
grantNixon <grntnixon1@gmail.com>
2025-09-12 10:18:19 -0500

    completed mystical weapons inventory functionality

--- WTTRPG/create/models.py
+++ WTTRPG/create/models.py
@@ -79,6 +79,7 @@ class Inventory(models.Model):
     clothing = models.JSONField(default=list)
     utilities = models.JSONField(default=list)
     tonics = models.JSONField(default=list)
+    mysticalweapons = models.JSONField(default=list)
 
 class Perk(models.Model):
     perkSkill = models.CharField(max_length=100)
